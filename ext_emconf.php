e3e3ac3801bae6cb3a4419774ddb297f51f0ffaa
Igor Neyman <in@points.de>
2022-08-04 16:55:05 +0200

    update ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,31 +10,25 @@
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
+$EM_CONF[$_EXTKEY] = [
   'title' => 'Pairs/Memory Card Game',
   'description' => 'Provide a flexible pairs game. You are able to configure the pairs game in many different types. You can assign two cards with the same image, two cards with similar images, two cards with an image and a description of the image or two text cards with the same description (i.e. in different languages). See an example on https://www.schulze-thulin.de/en/games/pairs-german-welsh-irish',
   'category' => 'plugin',
   'version' => '2.3.6',
   'state' => 'stable',
-  'uploadfolder' => true,
-  'createDirs' => '',
-  'clearcacheonload' => false,
+  'clearcacheonload' => 0,
   'author' => 'Gerald Loß',
   'author_email' => 'gerald.loss@gmx.de',
   'author_company' => '',
   'constraints' => 
-  array (
+  [
     'depends' => 
-    array (
-      'php' => '7.3.0-8.0.99',
-      'typo3' => '10.0.0-11.9.99',
-    ),
-    'conflicts' => 
-    array (
-    ),
-    'suggests' => 
-    array (
-    ),
-  ),
-);
+    [
+      'php' => '7.4.0-8.0.99',
+      'typo3' => '11.5.0-11.9.99',
+    ],
+    'conflicts' => [],
+    'suggests' => [],
+  ],
+];
 
